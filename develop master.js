ad4f7ccf00b14a3de2aa657e1e4242c6aa5ff8bc
E-compfast Digital Indonesia <58408252+e-compfast@users.noreply.github.com>
2020-11-08 09:23:53 +0700

    Rapihkan Penulisan Syntaxnya
    Untuk JSON agar mudah di pahami disarankan vertikal kebawah mas

--- develop master.js
+++ develop master.js
@@ -1,3 +1,18 @@
-var json_data = '{"profile" : "my account","Help","Setting","logout" : "notif": "pesan", "notifikasi" : "user": "",}';
+// var json_data = '{"profile" : "my account","Help","Setting","logout" : "notif": "pesan", "notifikasi" : "user": "",}';
 
-console.log(object.profile);
+// console.log(object.profile);
+
+
+const data : [
+  {
+  
+    profile: {
+      'nama' : 'alam'.
+      'email: 'alamhafidz01@gmail.com'
+    },
+    
+//     dan seterussnya
+
+  }
+
+]
